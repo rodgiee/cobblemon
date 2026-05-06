1ea91f24f97ca8d29d3f7aa0fc990d14ef0b84aa
rodgiee <rebecarodge@gmail.com>
2026-05-06 14:52:18 -0400

    refactor: medicinal brew to use generalization logic instead

--- ev_template/ev_template.lua
+++ ev_template/ev_template.lua
@@ -1,6 +1,7 @@
 local ev_template = {}
 
 local INGREDIENT_TO_OUTPUT = {
+  MEDICINAL_LEEK = "MEDICINAL_BREW",
   VIVICHOKE = "PP_UP",
   QUALOT_BERRY = "IRON",
   GREPA_BERRY = "ZINC",
@@ -63,6 +64,10 @@ local TARGET_SLOT = {
 }
 
 local SOURCE_SLOT = {
+  GLASS_BOTTLE = 1,
+  POTION_ONE = 2,
+  POTION_TWO = 3,
+  POTION_THREE = 4,
   BREW = 5,
 }
 
@@ -102,9 +107,38 @@ local function place_blaze_powder(brewing_stand, blaze_powder_container)
   end
 end
 
+-- TODO
+-- functional but not clean
+local function place_glass_bottle(potion_name)
+  local potion_count = 0
+  turtle.turnLeft()
+  for i = SOURCE_SLOT.POTION_ONE, SOURCE_SLOT.POTION_THREE do
+    local available_slot = get_last_available_slot(peripheral.wrap("front"))
+    if available_slot == -1 then
+      break
+    end
+
+    turtle.select(i)
+    turtle.suck(1)
+    turtle.placeDown()
+    potion_count = potion_count + 1
+  end
+
+  turtle.turnRight()
+  for i = SOURCE_SLOT.POTION_ONE, SOURCE_SLOT.POTION_ONE + potion_count do
+    turtle.select(i)
+    turtle.drop(1)
+  end
+  print("transferred " .. potion_count .. " " .. potion_name)
+  return potion_count
+end
+
 ---@param potion_container ccTweaked.peripheral.wrappedPeripheral
 ---@param brewing_stand ccTweaked.peripheral.wrappedPeripheral
 local function place_potion(potion_container, brewing_stand, potion_name)
+  if potion_name == "GLASS_BOTTLE" then -- medicinal brew recipe (requires filling water)
+    return place_glass_bottle(potion_container)
+  end
   local potion_count = 0
   for i = 1, 3 do
     local available_slot = get_last_available_slot(potion_container)
